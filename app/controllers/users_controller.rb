2fa1235d0008bfb65297c9d220c0a10a0b38e542
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-27 11:52:47 +0900

    Create CRUD function to User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+  before_action :user_set, only: [:show, :edit, :update, :destroy]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id), notice: "アカウントを作成しました"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "プロフィールを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to new_user_path, notice: "アカウントを削除しました"
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def user_set
+    @user = User.find(params[:id])
+  end
 end
